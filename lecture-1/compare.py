11117a9ae4538f486b4b78efa7c52462b0a47779
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-01 11:12:10 +0800

    use or

--- lecture-1/compare.py
+++ lecture-1/compare.py
@@ -1,9 +1,7 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x < y:
-    print("x is less than y")
-elif x > y:
-    print("x is greater than y")
+if x < y or x > y:
+    print("x is not equal to y")
 else:
     print("x is equal to y")
