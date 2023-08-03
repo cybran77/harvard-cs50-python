8e1b936e6b908882fab203a519cadf81253a4b96
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-03 12:32:15 +0800

    use a list of dictionaries

--- lecture-2/hogwarts.py
+++ lecture-2/hogwarts.py
@@ -3,11 +3,22 @@
 # for i in range(len(students)):
 #     print(i + 1, students[i])
 
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor"
-}
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor",
+#     "Ron": "Gryffindor"
+# }
+
+# for student in students:
+#     print(student, students[student], sep=", ")
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name": "Luna", "house": "Ravenclaw", "patronus": "Hare"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
 
 for student in students:
-    print(student, students[student], sep=", ")
+    print(student["name"], student["house"], student["patronus"], sep=", ")
