ce8d8df7dc31c08a9041a3440038bf4f0be59270
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-03 11:46:02 +0800

    validate inputs

--- lecture-2/cat.py
+++ lecture-2/cat.py
@@ -2,8 +2,25 @@
 # while i < 3:
 #     print("meow")
 #     i += 1
+# print("purr\n" * 3, end="")
 
-for _ in range(3):
-    print("meow")
 
-print("purr\n" * 3, end="")
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number():
+    while True:
+        n = int(input("Number of meows: "))
+        if n > 0:
+            return n
+
+
+def meow(n):
+    # print("meow\n" * n, end="")
+    for _ in range(n):
+        print("meow")
+
+
+main()
