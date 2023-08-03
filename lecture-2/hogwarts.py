1450248bbca0027e65141e55c92dee0618a61804
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-03 12:26:20 +0800

    use dictionary

--- lecture-2/hogwarts.py
+++ lecture-2/hogwarts.py
@@ -1,4 +1,13 @@
-students = ["Hermione", "Harry", "Ron"]
+# students = ["Hermione", "Harry", "Ron"]
 
-for i in range(len(students)):
-    print(i + 1, students[i])
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor"
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
