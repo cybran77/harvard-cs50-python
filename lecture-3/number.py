ec17deca803906b8d19104699b5b80b0b9e96538
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-04 16:52:17 +0800

    reprompting break

--- lecture-3/number.py
+++ lecture-3/number.py
@@ -1,7 +1,9 @@
 # value error
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("x must be an integer")
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = int(input("What's x? "))
+        break
+    except ValueError:
+        print("x must be an integer")
+
+print(f"x is {x}")
