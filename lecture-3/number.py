bc2cd300ac29006754fe9d9399e5e8a426eadf54
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-04 17:06:18 +0800

    finalize get_int

--- lecture-3/number.py
+++ lecture-3/number.py
@@ -1,9 +1,15 @@
-# value error
-while True:
-    try:
-        x = int(input("What's x? "))
-        break
-    except ValueError:
-        print("x must be an integer")
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
 
-print(f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            # print("x must be an integer")
+            pass
+
+
+main()
