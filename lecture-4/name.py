6cc7f47a824d8182c36647bdfb753825ae586010
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-07 12:38:12 +0800

    use slices

--- lecture-4/name.py
+++ lecture-4/name.py
@@ -1,8 +1,9 @@
 import sys
 
 if len(sys.argv) < 2:
-    print("Too few arguments")
-elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("hello, my name is", sys.argv[1])
+    sys.exit("Too few arguments")
+# elif len(sys.argv) > 2:
+#     sys.exit("Too many arguments")
+
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
