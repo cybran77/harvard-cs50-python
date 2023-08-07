4fc091fb1c5ab890bf6105f76d8c9129e95cb05f
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-07 12:13:17 +0800

    more random methods

--- lecture-4/generate.py
+++ lecture-4/generate.py
@@ -1,4 +1,12 @@
-from random import choice
+import random
 
-coin = choice(["heads", "tails"])
-print(coin)
+# coin = random.choice(["heads", "tails"])
+# print(coin)
+
+# number = random.randint(1, 10)
+# print(number)
+
+cards = ["jack", "queen", "king", "ace"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
