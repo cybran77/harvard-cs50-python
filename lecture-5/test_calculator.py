ec28fee886b602bff1a8429eca8d107085a1e2e7
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-11 14:54:16 +0800

    use pytest

--- lecture-5/test_calculator.py
+++ lecture-5/test_calculator.py
@@ -1,14 +1,9 @@
 from calculator import square
 
 
-def main():
-    test_square()
-
-
 def test_square():
     assert square(2) == 4
     assert square(3) == 9
-
-
-if __name__ == "__main__":
-    main()
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
