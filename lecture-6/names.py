c75474136b828f320d128af4a2fd017246d6c272
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-17 10:42:17 +0800

    use csv file

--- lecture-6/names.py
+++ lecture-6/names.py
@@ -15,15 +15,15 @@
 #     for line in file:
 #         print("hello,", line.rstrip())
 
-with open("names.txt") as file:
-    for line in sorted(file):
-        print(f"Hello, {line.rstrip()}!")
-
-# names = []
-
 # with open("names.txt") as file:
-#     for line in file:
-#         names.append(line.rstrip())
+#     for line in sorted(file):
+#         print(f"Hello, {line.rstrip()}!")
 
-# for name in sorted(names):
-#     print(f"Hello, {name}!")
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"Hello, {name}!")
