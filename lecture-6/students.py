18af91a6f77d5bb1253d610375a805840e5be5e6
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-17 11:17:43 +0800

    use csv lib

--- lecture-6/students.py
+++ lecture-6/students.py
@@ -1,19 +1,27 @@
+import csv
+
 students = []
 
+# with open("students.csv") as file:
+#     reader = csv.reader(file)
+#     for name, home in reader:
+#         students.append({"name": name, "home": home})
+
 with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        students.append({"name": name, "house": house})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
+
+for student in sorted(students, key=lambda s: s["name"]):
+    print(f"{student['name']} is in {student['home']}")
 
 
-def get_name(student):
-    return student["name"]
+# def get_name(student):
+#     return student["name"]
 
 
-def get_house(student):
-    return student["house"]
+# def get_home(student):
+#     return student["home"]
 
-
-# for student in sorted(students, key=lambda student: student["name"]):
-for student in sorted(students, key=get_name, reverse=True):
-    print(f"{student['name']} is in {student['house']}")
+# for student in sorted(students, key=get_name, reverse=True):
+#     print(f"{student['name']} is in {student['home']}")
