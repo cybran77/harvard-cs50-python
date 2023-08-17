4f1f794147e7cfe2845c267d009c3a092558bee2
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-08-17 10:58:22 +0800

    sort dictionary by keys

--- lecture-6/students.py
+++ lecture-6/students.py
@@ -1,4 +1,19 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        students.append({"name": name, "house": house})
+
+
+def get_name(student):
+    return student["name"]
+
+
+def get_house(student):
+    return student["house"]
+
+
+# for student in sorted(students, key=lambda student: student["name"]):
+for student in sorted(students, key=get_name, reverse=True):
+    print(f"{student['name']} is in {student['house']}")
