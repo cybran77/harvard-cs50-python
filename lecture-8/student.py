e96fb0dc6bf4a8e35884121d31349fe4dc9b3863
Fred Duazo <wilfredoduazojr@gmail.com>
2023-09-12 10:34:33 +0800

    use dictionary

--- lecture-8/student.py
+++ lecture-8/student.py
@@ -1,12 +1,14 @@
 def main():
     student = get_student()
-    print(f"{student[0]} from {student[1]}")
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name, house
+    return {"name": name, "house": house}
 
 
 if __name__ == "__main__":
