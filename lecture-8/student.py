bd57643f63c4efad7c9eb3c8a64f14aa20d88738
Fred Duazo <wilfredoduazojr@gmail.com>
2023-09-13 21:31:58 +0800

    add Student class

--- lecture-8/student.py
+++ lecture-8/student.py
@@ -1,14 +1,30 @@
+class Student:
+    def __init__(self, name, house=None):
+        if not name:
+            raise ValueError("Name is required")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("House is invalid")
+        self.name = name
+        self.house = house
+
+    # def get_name(self):
+    #     return self.name
+
+    # def get_house(self):
+    #     return self.house
+
+
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    if student.name == "Padma":
+        student.house = "Ravenclaw"
+    print(f"{student.name} from {student.house}")
 
 
-def get_student():
+def get_student():    
     name = input("Name: ")
     house = input("House: ")
-    return {"name": name, "house": house}
+    return Student(name, house)
 
 
 if __name__ == "__main__":
