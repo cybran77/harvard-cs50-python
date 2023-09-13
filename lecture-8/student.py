22bcf78fc66a206878a610e5b86a4a4d121106d8
Fred Duazo <wilfredoduazojr@gmail.com>
2023-09-13 22:24:01 +0800

    implement custom class method

--- lecture-8/student.py
+++ lecture-8/student.py
@@ -1,28 +1,39 @@
 class Student:
-    def __init__(self, name, house=None):
+    def __init__(self, name, house=None, patronus=None):
         if not name:
             raise ValueError("Name is required")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("House is invalid")
         self.name = name
         self.house = house
+        self.patronus = patronus.lower() if patronus else None
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+    def charm(self):
+        match self.patronus:
+            case "stag":
+                return "🦌"
+            case "otter":
+                return "🦦"
+            case "jack russell terrier":
+                return "🐶"
+            case _:
+                return "🐱"
+
 
 def main():
     student = get_student()
-    if student.name == "Padma":
-        student.house = "Ravenclaw"
-    # print(f"{student.name} from {student.house}")
-    print(student)
+    print("Expecto Patronum!")
+    print(student.charm())
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 
 if __name__ == "__main__":
