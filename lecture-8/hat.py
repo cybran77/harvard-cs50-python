547fcd1e1d891ca0f6335f561c76811360c1129d
Fred Duazo <wilfredoduazojr@gmail.com>
2023-09-14 09:58:01 +0800

    create class hat

--- lecture-8/hat.py
+++ lecture-8/hat.py
@@ -1,10 +1,18 @@
+import random
+
+
 class Hat:
-    ...
+    def __init__(self):
+        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    def sort(self, name):
+        house = random.choice(self.houses)
+        print(name, "is in", house)
 
 
 def main():
     hat = Hat()
-    print(hat)
+    hat.sort("Harry")
 
 
 if __name__ == "__main__":
