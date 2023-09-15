1a05f41afab3c8d735edb0556348ec0c0413517e
Fred Duazo <wilfredoduazojr@gmail.com>
2023-09-15 22:57:21 +0800

    implement method overloading

---
+++ lecture-8/vault.py
@@ -0,0 +1,26 @@
+class Vault:
+    def __init__(self, galleons=0, sickles=0, knuts=0):
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
+
+    def __str__(self):
+        return f"{self.galleons} galleons, {self.sickles} sickles, {self.knuts} knuts"
+
+    def __add__(self, other):
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Vault(galleons, sickles, knuts)
+
+
+potter = Vault(10, 20, 30)
+print(potter)
+
+weasley = Vault(10, 20, 100)
+print(weasley)
+
+slytherin = Vault(10, 20, 100)
+
+total = potter + weasley + slytherin
+print(total)
