ada4767314b27d04898f0030486bd16d3e0b2624
Fred Duazo <wilfredoduazojr@gmail.com>
2023-09-15 22:10:27 +0800

    use class methods or static

--- lecture-8/hat.py
+++ lecture-8/hat.py
@@ -2,18 +2,11 @@ import random
 
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
-    def sort(self, name):
-        house = random.choice(self.houses)
-        print(name, "is in", house)
-
-
-def main():
-    hat = Hat()
-    hat.sort("Harry")
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
 
 
-if __name__ == "__main__":
-    main()
+Hat.sort("Harry")
